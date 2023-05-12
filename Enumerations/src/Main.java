77b5063cc97a1879466746c30291c2ebc25cc75a
AEGONTR\ataberk.cetinkaya <ataberk.cetinkaya@viennalife.com.tr>
2023-05-12 17:25:40 +0300

    Enums

---
+++ Enumerations/src/Main.java
@@ -0,0 +1,29 @@
+import java.time.DayOfWeek;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Months months = Months.JANUARY;
+		System.out.println(months);
+
+		months = Months.MARCH;
+		System.out.println(months);
+		
+		System.out.println("------------------------");
+		
+		Months[] theMonths = Months.values();
+		for(int i = 0; i < theMonths.length; i++) {
+			System.out.println(theMonths[i]);
+		}
+		
+		System.out.println("------------------------");
+		
+		DayOfWeek sunday = DayOfWeek.MONDAY; //MONDAY
+		
+		DayOfWeek indexOf = DayOfWeek.of(3); //WEDNESDAY
+		System.out.println(indexOf);
+		
+		//MONTH enum also available
+	}
+
+}
