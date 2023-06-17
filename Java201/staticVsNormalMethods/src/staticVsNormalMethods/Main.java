bd94613dc6881553d02f7dcac43bf6470783ef13
ataberk <ataberkcetinkaya@outlook.com>
2023-06-17 23:28:46 +0300

    Methods Lab 2

--- Java201/staticVsNormalMethods/src/staticVsNormalMethods/Main.java
+++ Java201/staticVsNormalMethods/src/staticVsNormalMethods/Main.java
@@ -1,5 +1,7 @@
 package staticVsNormalMethods;
 
+import java.util.Arrays;
+
 public class Main {
 	
 	private static int x = 20; //that must be static variable too, if we want to reach it from a static method.
@@ -15,6 +17,20 @@ public class Main {
 		
 		double dMax = Main.max(2.0, 5.0, 3.0);
 		System.out.println("The highest double is: " + dMax);
+		
+		boolean majority = Main.majority(true, false, true);
+		System.out.println("The majority is: " + majority);
+		
+		int[] x = {1,2,3};
+		int[] y = {1,2,4};
+		System.out.println("the arrays are exactly same? " + Main.eq(x, y));
+	
+		System.out.println("Cube of the number is: " + Main.cube(2));
+		
+		System.out.println("Total is: " + Main.sum(x));
+		
+		int[] findingTheEvens = Main.evenNumbers(new int[] {1,2,3,4,5,6,7,8});
+		System.out.println("Even numbers are: " + Arrays.toString(findingTheEvens));
 	}
 	
 	public void normalMethod() {
@@ -46,4 +62,62 @@ public class Main {
 		}
 		return max;
 	}
+	
+	public static boolean majority(boolean x, boolean y, boolean z) {
+		return (x && y) || (x && z) || (y && z); //if any of two parameters comes true, will show up the result as true
+	}
+	
+	public static boolean eq(int[] x, int[] y) { //this method will compare the int arrays (100% same or not)
+		boolean result = true;
+		
+		if(x.length == y.length) {
+			for(int i = 0; i < x.length; i++) {
+				if(x[i] != y[i]) {
+					result = false;
+					break;
+				}
+			}
+		}
+		else {
+			result = false;
+		}
+		
+		return result;
+	}
+	
+	public static int cube(int x) {
+		return x * x * x;
+	}
+	
+	public static int sum(int[] x) {
+		int total = 0;
+		for(int i = 0; i < x.length; i++) {
+			total +=  x[i];
+		}
+		return total;
+	}
+	
+	public static int[] evenNumbers(int[] numbers) {
+	    int count = 0;
+	    for(int i = 0; i < numbers.length; i++) { //iterate through the array and find the even numbers also increase the count
+	        if (numbers[i] % 2 == 0) {
+	            count++;
+	        }
+	    }
+	    int[] theEvens = new int[count]; //we use the count variable to create another array.
+	    int index = 0; //will hold the place of even numbers
+	    for(int i = 0; i < numbers.length; i++) {
+	        if (numbers[i] % 2 == 0) { //iterate through the numbers array again and find the evens
+	            theEvens[index] = numbers[i]; //and at these numbers to the theEvens array we just created.
+	            index++; 
+	            //theEvens[0] = numbers[2];
+	            //theEvens[1] = numbers[4];
+	            //theEvens[2] = numbers[6];
+	        }
+	    }
+	    return theEvens;
+	}
+
+	
+	
 }
